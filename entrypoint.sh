1addabacb968e51ac4bd211341ec9bf1d0c5a9e9
Bright Kidiga <56060388+Bright-kidiga@users.noreply.github.com>
2025-08-19 10:36:51 +0300

    update: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,22 +8,17 @@ cd $FRAPPE_HOME/frappe-bench
 : "${DB_ROOT_PASSWORD:?Need to set DB_ROOT_PASSWORD}"
 : "${ADMIN_PASSWORD:?Need to set ADMIN_PASSWORD}"
 
-# Wait for MariaDB to start using Python + PyMySQL
-echo "Waiting for MariaDB to start..."
-until python3 -c "import pymysql; pymysql.connect(host='127.0.0.1', user='root', password='$DB_ROOT_PASSWORD')" >/dev/null 2>&1; do
-    echo "MariaDB not ready yet... sleeping 2 seconds"
-    sleep 2
-done
+TABLE_EXISTS=$(mysql -u"$DB_USER" -p"$DB_PASSWORD" -h"$DB_HOST" -D"$DB_NAME" -e "SHOW TABLES LIKE 'tabSingles';" -s --skip-column-names)
 
 # Check if site already exists
-if [ ! -d "sites/$SITE_NAME" ]; then
+if [ -z "$TABLE_EXISTS" ]; then
     echo "Site $SITE_NAME does not exist. Creating..."
     bench new-site "$SITE_NAME" \
         --db-root-username root \
-        --db-root-password "$DB_ROOT_PASSWORD" \
+        --db-root-password "$DB_PASSWORD" \
         --admin-password "$ADMIN_PASSWORD"
 else
-    echo "Site $SITE_NAME already exists. Skipping creation."
+    echo "Site already exists. Skipping creation."
 fi
 
 # Start bench
